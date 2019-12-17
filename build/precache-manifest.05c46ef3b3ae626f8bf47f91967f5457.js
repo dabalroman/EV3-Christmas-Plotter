74afe0d903bbf1d75482351113481360b6ba148c
Roman Dąbal <dabalroman@gmail.com>
2019-12-17 13:23:45 +0100

    Build 1.1.0 (all files)

---
+++ build/precache-manifest.05c46ef3b3ae626f8bf47f91967f5457.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64f028767ae54c8de5433df2075223d7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e081c27aed99e11eccfb",
+    "url": "./static/css/main.dcd0f17d.chunk.css"
+  },
+  {
+    "revision": "b5b45bcbb5741887cd3a",
+    "url": "./static/js/2.f37bf163.chunk.js"
+  },
+  {
+    "revision": "e081c27aed99e11eccfb",
+    "url": "./static/js/main.ef3d7a06.chunk.js"
+  },
+  {
+    "revision": "769e278c022d2fa2b00c",
+    "url": "./static/js/runtime-main.479117b2.js"
+  },
+  {
+    "revision": "5de77feb9b7c04ca5a278dfabedba794",
+    "url": "./static/media/CodePasteIntoEditor.5de77feb.mp4"
+  },
+  {
+    "revision": "2c1dbfd6c9c123d59ca1998a046a322c",
+    "url": "./static/media/Drawing.2c1dbfd6.mp4"
+  },
+  {
+    "revision": "7ad3dcd35f2a4aa9ae17dbe5727d67a3",
+    "url": "./static/media/icons.7ad3dcd3.svg"
+  },
+  {
+    "revision": "94f6a1d42df8530da2908fac93c7f652",
+    "url": "./static/media/icons.94f6a1d4.ttf"
+  },
+  {
+    "revision": "9d25156c93bc4d5308eb35d88529b9af",
+    "url": "./static/media/icons.9d25156c.woff"
+  },
+  {
+    "revision": "9f6d98ace3677b2f8046011ae8604d11",
+    "url": "./static/media/icons.9f6d98ac.woff2"
+  },
+  {
+    "revision": "d272cdfb159ac361dbccc95ade3527e3",
+    "url": "./static/media/icons.d272cdfb.eot"
+  }
+]);
